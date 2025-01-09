c330888654369bac92e92cb01ee8000ed662cda2
ccubias487 <ccubias487@gmail.com>
2025-01-09 17:01:00 -0600

    Update bodega_existencias.js

--- bodega_existencias.js
+++ bodega_existencias.js
@@ -4,6 +4,9 @@ document.getElementById("Buscar_string").addEventListener("keydown", function (e
     event.preventDefault();
   }
 });
+if (localStorage.getItem("usuario")== null){
+    window.location.href = "index.html";
+  }
 
 function numeroAleatorio(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
